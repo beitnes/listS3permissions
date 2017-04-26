26910ed09f973f251f61127ff4e68342eb1b4c11
Anders Beitnes <beitnes@yahoo-inc.com>
2017-04-26 15:53:52 -0700

    argv parsing fix

--- listS3acls.py
+++ listS3acls.py
@@ -7,9 +7,11 @@ import pprint
 
 def main():
     s3 = boto3.client('s3')
-    buckets = sys.argv[1].split()
+    buckets = sys.argv[1:]
+    print(buckets)
 
     for bucket in buckets:
+        print(bucket)
         response = s3.get_bucket_acl(Bucket=bucket)
         pprint.PrettyPrinter(indent=4).pprint(response)
 
