c09e49dfd8cd8dd9f61844c52c72383b24f05950
Anders Beitnes <beitnes@yahoo-inc.com>
2017-04-26 20:31:10 -0700

    cleanup print statements, use anaconda

--- listS3acls.py
+++ listS3acls.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/anaconda/bin/python
 
 import sys
 import boto3
@@ -8,10 +8,8 @@ import pprint
 def main():
     s3 = boto3.client('s3')
     buckets = sys.argv[1:]
-    print(buckets)
 
     for bucket in buckets:
-        print(bucket)
         response = s3.get_bucket_acl(Bucket=bucket)
         pprint.PrettyPrinter(indent=4).pprint(response)
 
